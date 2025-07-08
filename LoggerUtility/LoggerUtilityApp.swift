dc70cb5a3ed5e61ee4efac071900cdc11d0256bf
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-08 16:21:48 +0300

    Initial Commit

---
+++ LoggerUtility/LoggerUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoggerUtilityApp.swift
+//  LoggerUtility
+//
+//  Created by Tahir Anil Oghan on 8.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoggerUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoggerUtilityTests/LoggerUtilityTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoggerUtilityTests.swift
+//  LoggerUtilityTests
+//
+//  Created by Tahir Anil Oghan on 8.07.2025.
+//
+
+import Testing
+@testable import LoggerUtility
+
+struct LoggerUtilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
